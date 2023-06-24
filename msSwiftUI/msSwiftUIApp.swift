d86938bb44c995682aca2ed1e784687cfb23c127
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-06-24 08:01:36 -0500

    Initial Commit

---
+++ msSwiftUI/msSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  msSwiftUIApp.swift
+//  msSwiftUI
+//
+//  Created by Jeisson Chacón on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct msSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
