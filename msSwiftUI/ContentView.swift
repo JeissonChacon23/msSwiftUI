099b46da461d62a4c937a0460ae84d6ef340210c
Jeisson Chacón <Jeissonchaconjp@gmail.com>
2023-06-24 17:37:00 -0500

    To make text larger, insert an image

--- msSwiftUI/ContentView.swift
+++ msSwiftUI/ContentView.swift
@@ -15,7 +15,18 @@ struct ContentView: View {
         NavigationView{
             List{
                 ForEach(nombres, id:\.self){
-                    nombre in Text("\(nombre)")
+                    nombre in
+                    HStack {
+                        VStack(alignment: .leading) {//Command + Click --> Show SwiftUI Ispector
+                            Text("\(nombre)")
+                                .font(.headline)
+                            Text("Hola")
+                                .font(.footnote)
+                        }
+                        Spacer()
+                        Image(systemName: "pencil")
+                            .font(.title) //To make the text larger
+                    }
                 }
             }
             .navigationTitle("Full Name")
